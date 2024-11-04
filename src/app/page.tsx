020ff10aaa6365ed342a1854d109bbd333d9b984
Sam McClenghan <sam@aream.ca>
2024-11-04 14:45:06 -0800

    Connected database to schema with drizzle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,21 @@
-const imageURLs = [
-  "https://utfs.io/f/vO3Q1z5KV9G7dJOAquHtM3IUvF9xnE2upjYzsK1lroAN54mO",
-  "https://utfs.io/f/vO3Q1z5KV9G7jlfvGj8uswlPNz5Ue3bX8rvkdx6KDtCYSmMa",
-  "https://utfs.io/f/vO3Q1z5KV9G7ZeFMjxE3IlUcfkSjp7V4zLbXnsHrPveqGihD",
-  "https://utfs.io/f/vO3Q1z5KV9G7kWbsVwCl5eE036FK2ztnLIDUXA89cm4vYRdj",
-];
+import { db } from "~/server/db";
 
-const mockImages = imageURLs.map((url, index) => ({
-  id: index + 1,
-  url,
-}))
+export const dynamic = "force-dynamic"
 
-export default function HomePage() {
+export default async function HomePage() {
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id)
+  })
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
-          <div key={image.id} className="w-48">
+        {images.map((image, index) => (
+          <div key={image.id + "-" + index} className="w-48">
             <img src={image.url} />
+            <div>{image.name}</div>
           </div>
         ))}
       </div>
-    </main>
+    </main >
   );
 }
