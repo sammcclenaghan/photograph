8709f885f4bbacaa217888688faca921b349ae88
Sam McClenghan <sam@aream.ca>
2024-11-03 20:42:30 -0800

    Implemented Header

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <nav className="flex w-full items-center justify-between p-4 text-xl border-b font-semibold">
+      <div> Gallery</div>
+      <div>Sign in</div>
+    </nav>
+  )
+}
