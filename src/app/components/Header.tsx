e5d3fe55bc6a0fe7a4f23b0b2383b8380746d68f
Sam McClenghan <sam@aream.ca>
2024-11-04 15:20:44 -0800

    Added clerk auth

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,8 +1,20 @@
+import {
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
+
 export default function Header() {
   return (
     <nav className="flex w-full items-center justify-between p-4 text-xl border-b font-semibold">
       <div> Gallery</div>
-      <div>Sign in</div>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </nav>
   )
 }
