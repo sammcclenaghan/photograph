147d115e7368563795c70ab7f68b25195782775f
Sam McClenghan <sam@aream.ca>
2024-11-09 16:39:53 -0800

    Styling modal

--- src/app/components/PhotoPage.tsx
+++ src/app/components/PhotoPage.tsx
@@ -1,14 +1,28 @@
 import { getImage } from "~/server/queries";
+import Image from "next/image";
+interface PhotoPageProps {
+  id: number;
+}
 
-export default async function PhotoPage(props: { id: number }) {
-  const image = await getImage(props.id)
+export default async function PhotoPage(props: PhotoPageProps) {
+  const image = await getImage(props.id);
+
   return (
-    <div className="flex w-full h-full min-w-0 bg-green-300">
-      <div className="flex-shrink flex items-center justify-center">
-        <img src={image.url} className="flex-shrink object-contain" />
+    <div className="flex flex-row w-full h-screen bg-green-300">
+      {/* Image Container */}
+      <div className="w-2/3 flex items-center justify-center p-2">
+        <img
+          src={image.url}
+          alt={image.name}
+          className="max-w-full max-h-full object-contain"
+        />
       </div>
-      <div className="flex w-48 flex-col">
-        <div className="text-xl font-bold">{image.name}</div>
+
+      {/* Content Container */}
+      <div className="w-1/3 flex items-center justify-center p-4">
+        <div className="text-xl font-bold">
+          {image.name}
+        </div>
       </div>
     </div>
   );
