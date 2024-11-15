9cf617b31d2fe2dfb17a7bdc81f4a7055702e5f3
Sam McClenghan <sam@aream.ca>
2024-11-15 12:14:32 -0800

    Fixed signed out galleries error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,22 +7,29 @@ import { createGallery } from "~/server/actions";
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
-  const galleries = await getGalleries();
-
   return (
     <main className="mx-auto max-w-[1960px] p-4">
       <SignedOut>
         <LandingPage />
       </SignedOut>
       <SignedIn>
-        {/* Header with Create Gallery Button */}
-        <div className="flex justify-between items-center mb-6">
-          <h1 className="text-2xl font-bold">My Galleries</h1>
-          <CreateGalleryForm />
-        </div>
-        {/* Galleries Grid */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-          {galleries.map((gallery) => (
+        <Galleries />
+      </SignedIn>
+    </main>
+  );
+}
+
+async function Galleries() {
+  const galleries = await getGalleries();
+  return (
+    <div>
+      < div className="flex justify-between items-center mb-6" >
+        <h1 className="text-2xl font-bold">My Galleries</h1>
+        <CreateGalleryForm />
+      </div >
+      < div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6" >
+        {
+          galleries.map((gallery) => (
             <div key={gallery.id} className="flex flex-col">
               <Link href={`/gallery/${gallery.id}`}>
                 <div className="relative w-full h-0 pb-[100%]">
@@ -31,11 +38,11 @@ export default async function HomePage() {
               </Link>
               <div className="mt-2 text-center">{gallery.name}</div>
             </div>
-          ))}
-        </div>
-      </SignedIn>
-    </main>
-  );
+          ))
+        }
+      </div >
+    </div>
+  )
 }
 
 function CreateGalleryForm() {
